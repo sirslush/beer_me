3bfa0142b8efd5404910821cd82eccbe38ab3266
Alec Foster <alecfoster@Alecs-MacBook-Pro.local>
2020-04-15 18:56:49 -0700

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const someFunction = () => {
+
+}
+
+const addBeerToList = function(value){
+  const url = "https://api.punkapi.com/v2/beers/" + value
+    const beerList = document.getElementsByClassName('beer__id__list')[0];
+    const newListItem = document.createElement('li');
+    const newListLink = document.createElement('a');
+
+    fetch(url)
+    .then(response => response.json())
+    .then(beers => {
+      const beer = beers[0];
+      newListLink.innerHTML = beer.name;
+
+    });
+    newListItem.appendChild(newListLink);
+    beerList.appendChild(newListItem);
+};
+
+
 const grabRandomBeer = () => {
   // Fetching random beer data from API
   fetch("https://api.punkapi.com/v2/beers/random")
@@ -12,9 +34,11 @@ const grabRandomBeer = () => {
       descriptionElement.textContent = beer.description;
       // Clear previous beer data
       const beerElement = document.getElementById("beer");
+      beerElement.innerHTML = "";
       // Add beer info to the page
       beerElement.appendChild(nameElement);
       beerElement.appendChild(descriptionElement);
+      addBeerToList(beer.id);
     })
     .catch(err => {
       console.error(err.message);
@@ -24,4 +48,9 @@ const grabRandomBeer = () => {
 const buttons = document.getElementsByClassName('beer__button');
 for (const button of buttons) {
   button.addEventListener("click", grabRandomBeer);
+}
+
+const listItems =document.getElementsByClassName('beer__list__item');
+for (const item of listItems) {
+  item.addEventListener('click', someFunction);
 }
