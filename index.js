f79fcc370f08d87e9abc5ab72036257a95023a48
Alec Foster <sirslush@live.com>
2020-04-16 17:07:22 -0700

    trying to fix second function

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const someFunction = (idNum) => {
   const url = "https://api.punkapi.com/v2/beers/" + idNum;
-    const beerList = document.getElementsByClassName('beer__id__list')[0];
-    const newListItem = document.createElement('li');
-    const newListLink = document.createElement('a');
 
     fetch(url)
     .then(response => response.json())
     .then(beers => {
+      // API returns an array containg only one element: we get it
       const beer = beers[0];
-      newListLink.innerHTML = beer.name;
-
+      alert("function called")
+      
+      // Creating DOM element for some beer properties
+      const nameElement = document.createElement("h2");
+      nameElement.textContent = beer.name;
+      const descriptionElement = document.createElement("p");
+      descriptionElement.textContent = beer.description;
+      // Clear previous beer data
+      const beerElement = document.getElementById("beer");
+      beerElement.innerHTML = "";
+      // Add beer info to the page
+      beerElement.appendChild(nameElement);
+      beerElement.appendChild(descriptionElement);
     });
 
 }
@@ -20,12 +29,14 @@ const addBeerToList = function(value){
     const newListItem = document.createElement('li');
     const newListLink = document.createElement('a');
 
+    newListItem.classList.add("beer__list__item");
+
     fetch(url)
     .then(response => response.json())
     .then(beers => {
       const beer = beers[0];
       newListLink.innerHTML = beer.name;
-
+      newListItem.addEventListener('click', someFunction(beer.id));
     });
     newListItem.appendChild(newListLink);
     beerList.insertBefore(newListItem, beerList.childNodes[1]);
@@ -62,7 +73,3 @@ for (const button of buttons) {
   button.addEventListener("click", grabRandomBeer);
 }
 
-const listItems =document.getElementsByClassName('beer__list__item');
-for (const item of listItems) {
-  item.addEventListener('click', someFunction);
-}
