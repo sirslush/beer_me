3c5305ed622469b0ef9bb0e90dad54ed0d0fd113
Alec Foster <sirslush@live.com>
2020-04-18 08:58:35 -0700

    catching up master to changes made on Desktop

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-const someFunction = (url) => {
+const removeBeerListItem = function(){
+  const beerList = document.getElementsByClassName('beer__list__items')[0];
+  beerList.removeChild(beerList.childNodes[beerList.childElementCount]);
+}
+
+const showBeerInfo = (beer) =>{
+  const nameElement = document.createElement("h2");
+  nameElement.textContent = beer.name;
+  const descriptionElement = document.createElement("p");
+  descriptionElement.textContent = beer.description;
+
+  // const beerImg = document.createElement('img');  // Img data not accurrate and will be implimented later
+  // beerImg.src = beer.image_url;
+
+  // Clear previous beer data
+  const beerElement = document.getElementsByClassName("beer__info__text")[0];
+  beerElement.innerHTML = "";
+  // Add beer info to the page
+  beerElement.appendChild(nameElement);
+  beerElement.appendChild(descriptionElement);
+  // beerElement.appendChild(beerImg);  Img data not accurate and will be implimented later
+}
+
+const showBeerInfoFromList = (url) => {
     fetch(url)
     .then(response => response.json())
     .then(beers => {
       // API returns an array containg only one element: we get it
       const beer = beers[0];
-      // Creating DOM element for some beer properties
-      const nameElement = document.createElement("h2");
-      nameElement.textContent = beer.name;
-      const descriptionElement = document.createElement("p");
-      descriptionElement.textContent = beer.description;
-      // Clear previous beer data
-      const beerElement = document.getElementById("beer");
-      beerElement.innerHTML = "";
-      // Add beer info to the page
-      beerElement.appendChild(nameElement);
-      beerElement.appendChild(descriptionElement);
+      showBeerInfo(beer);
     });
 
 }
 
 const addBeerToList = function(value){
   const url = "https://api.punkapi.com/v2/beers/" + value;
-    const beerList = document.getElementsByClassName('beer__id__list')[0];
+    const beerList = document.getElementsByClassName('beer__list__items')[0];
     const newListItem = document.createElement('li');
     const newListLink = document.createElement('div');
 
@@ -33,7 +46,7 @@ const addBeerToList = function(value){
       const beer = beers[0];
       newListLink.innerHTML = beer.name;
       newListLink.addEventListener("click", function() {
-        someFunction(url);
+        showBeerInfoFromList(url);
     }, false);
     });
     newListItem.appendChild(newListLink);
@@ -48,18 +61,12 @@ const grabRandomBeer = () => {
     .then(beers => {
       // API returns an array containg only one element: we get it
       const beer = beers[0];
-      // Creating DOM element for some beer properties
-      const nameElement = document.createElement("h2");
-      nameElement.textContent = beer.name;
-      const descriptionElement = document.createElement("p");
-      descriptionElement.textContent = beer.description;
-      // Clear previous beer data
-      const beerElement = document.getElementById("beer");
-      beerElement.innerHTML = "";
-      // Add beer info to the page
-      beerElement.appendChild(nameElement);
-      beerElement.appendChild(descriptionElement);
+      showBeerInfo(beer);
       addBeerToList(beer.id);
+      const beerList = document.getElementsByClassName('beer__list__item');
+      if(beerList.length>5){
+        removeBeerListItem();
+      }
     })
     .catch(err => {
       console.error(err.message);
@@ -70,4 +77,6 @@ const buttons = document.getElementsByClassName('beer__button');
 for (const button of buttons) {
   button.addEventListener("click", grabRandomBeer);
 }
+
+
 
